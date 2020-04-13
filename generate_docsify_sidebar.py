cc698eaffe1156c015a509cc4c1da3d53334930c
Erimus-Koo <chuangqianmingyueguang+github@gmail.com>
2020-04-13 23:21:17 +0800

    do not update without change

--- generate_docsify_sidebar.py
+++ generate_docsify_sidebar.py
@@ -8,45 +8,61 @@ __author__ = 'Erimus'
 import os
 
 # ═══════════════════════════════════════════════
+md = md_private = ''
+# ═══════════════════════════════════════════════
+
+
+def add_content(fullpath):
+    global md, md_private
+    fullpath = fullpath.replace('\\', '/')
+    fullpath = fullpath[len(root):].lstrip('/')
+    if not fullpath:
+        return  # skip root
+    indent = fullpath.count('/') * '    '  # indent level
+    levels = f'/{fullpath}'.split('/')  # per folder in path
+    filename = levels[-1]  # get file/folder name
+    # ignore sidebar file
+    if filename.startswith('_sidebar') or '.assets' in fullpath:
+        return
+    filename = filename[0] + filename[1:].replace('_', ' ')
+
+    # generate line
+    if fullpath.endswith('.md'):
+        line = f'{indent}- [**{filename[:-3]}**]({fullpath})'
+    else:  # folder
+        line = f'{indent}- **{filename}**'
+
+    # add line to public md
+    for name in levels:
+        if name.startswith('_'):
+            break
+    else:  # all path(self or parent) is not private
+        md += f'{line}\n'
+
+    # add line to private
+    line = line.replace('**_', '**')  # remove private underscore in display
+    md_private += f'{line}\n'
+
+
+def update_file(file, content):
+    with open(file, 'r', encoding='utf-8') as f:
+        old = f.read()
+    fn = file.split('\\')[-1]
+    if old.strip() != content.strip():
+        with open(file, 'w', encoding='utf-8') as f:
+            f.write(content)
+        print(f'Updated {fn}')
+    else:
+        print(f'{fn} - has no change')
 
 
 def generate_sidebar(root=None, ignoreFolders=None):
+    global md, md_private
     ignoreFolders = [] if ignoreFolders is None else ignoreFolders
     if root is None:
         root = os.getcwd()  # the path of command line
 
-    md = []  # result md
-    md_private = []
-
-    def add_content(fullpath):
-        fullpath = fullpath.replace('\\', '/')
-        fullpath = fullpath[len(root):].lstrip('/')
-        if not fullpath:
-            return  # skip root
-        indent = fullpath.count('/') * '    '  # indent level
-        levels = f'/{fullpath}'.split('/')  # per folder in path
-        filename = levels[-1]  # get file/folder name
-        # ignore sidebar file
-        if filename.startswith('_sidebar') or '.assets' in fullpath:
-            return
-        filename = filename[0] + filename[1:].replace('_', ' ')
-
-        # generate line
-        if fullpath.endswith('.md'):
-            line = f'{indent}- [**{filename[:-3]}**]({fullpath})'
-        else:  # folder
-            line = f'{indent}- **{filename}**'
-
-        # add line to public md
-        for name in levels:
-            if name.startswith('_'):
-                break
-        else:  # all path(self or parent) is not private
-            md.append(line)
-
-        # add line to private
-        line = line.replace('**_', '**')  # remove private underscore in display
-        md_private.append(line)
+    md = md_private = ''
 
     for path, dirs, files in os.walk(root):  # read all files
         dirs[:] = [d for d in dirs if d not in ignoreFolders]
@@ -60,21 +76,16 @@ def generate_sidebar(root=None, ignoreFolders=None):
                 # add md file
                 add_content(os.path.join(path, fn))
 
-    print('_sidebar.md\n')
-    [print(i) for i in md]
-    print('\n-----\n\n_sidebar_private.md\n')
-    [print(i) for i in md_private]
+    # print(f'_sidebar.md\n{md}\n-----\n\n_sidebar_private.md\n{md_private}')
 
-    with open(os.path.join(root, '_sidebar.md'), 'w', encoding='utf-8') as f:
-        f.write('\n'.join(md))
-    with open(os.path.join(root, '_sidebar_private.md'), 'w', encoding='utf-8') as f:
-        f.write('\n'.join(md_private))
+    update_file(os.path.join(root, '_sidebar.md'), md)
+    update_file(os.path.join(root, '_sidebar_private.md'), md_private)
 
 
 # ═══════════════════════════════════════════════
 
 if __name__ == '__main__':
 
-    root = 'D:/OneDrive/erimus-koo.github.io/notebook'  # my docsify root
+    root = r'D:\OneDrive\site\notebook'  # my docsify root
     ignoreFolders = ['.git', '上海通']
     generate_sidebar(root, ignoreFolders)
