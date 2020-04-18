cedf76cc014fd63e96e79147d7267a0116abc8d9
Erimus-Koo <chuangqianmingyueguang+github@gmail.com>
2020-04-18 17:44:25 +0800

    运动提醒

---
+++ gym_timer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'Erimus'
+# 定期起来动一动
+
+from erimus.toolbox import *
+import fire
+
+# ═══════════════════════════════════════════════
+
+
+def gym_timer(gap=15):
+    gap *= 60  # minutes to seconds
+    for i in range(99):
+        print(f'[{formatTime()}] Count: {i+1}')
+        beep(1, 'gym_time.mp3')
+        now = timestamp()
+        countdown(now // gap * gap + gap - now)
+
+
+# ═══════════════════════════════════════════════
+
+
+if __name__ == '__main__':
+
+    fire.Fire(gym_timer)
