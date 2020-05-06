8cf950c9c87c33652e040ea7a3de596324ca206c
Erimus-Koo <chuangqianmingyueguang+github@gmail.com>
2020-05-06 18:33:02 +0800

    + print main PID

--- autorun.py
+++ autorun.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Erimus'
 
+from erimus.toolbox import *
 from multiprocessing import Pool
-import os
 from util.remote_key_service import init_server as remote_key
 from util.scheduled_tasks import scheduled_tasks
 
@@ -13,37 +13,43 @@ cmd_list = [
     # r'python D:\OneDrive\05ProgramProject\Python\utilities\share\remote_key_service.py',
     # r'python D:\OneDrive\05ProgramProject\Python\utilities\share\scheduled_tasks.py'
 ]
+MAIN_PID = os.getpid()
 # ═══════════════════════════════════════════════
 
 
-def run_cmd(cmd):  # 套壳会导致python进程多一层 后台看着有点乱
-    print(f'[{os.getpid()}] {cmd}')  # 第一层
-    os.system(cmd)  # 第二层
+# def run_cmd(cmd):  # 套壳会导致python进程多一层 后台看着有点乱
+#     print(f'[{MAIN_PID}] {cmd}')  # 第一层
+#     os.system(cmd)  # 第二层
 
 
 def http_server(): return os.system(r'python -m http.server 80 --directory D:\OneDrive\site\ --bind 10.0.0.13')
 
 
+def print_main_pid(MAIN_PID):
+    while True:
+        print(FS.title(f'[{os.getpid()}] AUTORUN Main PID is {MAIN_PID}'))
+        time.sleep(600)
+
+
 func_list = [
-    http_server,
-    remote_key,
-    scheduled_tasks
+    (print_main_pid, (MAIN_PID,)),
+    (http_server, ()),
+    (remote_key, ()),
+    (scheduled_tasks, ()),
 ]
 
 
 def main():
-    print(f'[{os.getpid()}] AUTORUN')
     p = Pool(len(cmd_list + func_list))  # 设置进程数
-    for cmd in cmd_list:
-        p.apply_async(run_cmd, (cmd,))
-    for func in func_list:
-        p.apply_async(func, ())
+    # for cmd in cmd_list:
+    #     p.apply_async(run_cmd, (cmd,))
+    for func, param in func_list:
+        p.apply_async(func, param)
     p.close()
     p.join()
 
 
 # ═══════════════════════════════════════════════
-
 if __name__ == '__main__':
 
     main()
