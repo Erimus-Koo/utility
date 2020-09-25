40bce5559528658f815a8f1bc0a33b2d62a5741c
Erimus-Koo <chuangqianmingyueguang+github@gmail.com>
2020-09-25 23:42:58 +0800

    ts直接拼接 并缩短命令长度

--- merge_video.py
+++ merge_video.py
@@ -11,48 +11,38 @@ import fire
 
 
 def merge_video(root=None):
-    root = os.getcwd() if root is None else root
+    video_root = os.getcwd() if root is None else root
+    print(f'{video_root = }')
 
+    out = None
     video_dict = {}
     # find video file
-    for path, dirs, files in os.walk(root):  # read all files
+    for path, dirs, files in os.walk(video_root):  # read all files
         for fn in files:
             name, ext = os.path.splitext(fn)
-            if ext in ['.mp4', '.webm', '.flv']:
-                if name.endswith('[00]'):
-                    video_dict[name[:-4]] = {'video': '', 'audio': '',
-                                             'sub': [], 'ext': ext}
-
+            if out is None:
+                out = os.path.join(path, name + '_merge' + ext)
+            if ext in ('.mp4', '.webm', '.flv', '.ts'):
+                video_dict.setdefault(ext, [])
+                if root is None:
+                    video_dict[ext].append(fn)
+                else:
+                    video_dict[ext].append(os.path.join(video_root, fn))
         break  # root only
-
-    # find all assets of video
-    for path, dirs, files in os.walk(root):  # read all files
-        for fn in files:
-            for video_name, info in video_dict.items():
-                if fn.startswith(video_name):
-                    name, ext = os.path.splitext(fn)
-                    if name[-4:] == '[00]':
-                        info['video'] = fn
-                    elif name[-4:] == '[01]':
-                        info['audio'] = fn
-                    elif ext == '.srt':
-                        info['sub'].append(fn)
-
-        break  # root only
-
-    print(video_dict.keys())
 
     # merge video
-    for video_name, info in video_dict.items():
-        if not (info['video'] and info['audio']):  # 必须音视频齐全
-            continue
-        assets = [info['video'], info['audio']] + info['sub']
-        out = video_name + info['ext']
-        info_cmd = ''.join([f' -i "{f}"' for f in assets])
-
-    cmd = f'ffmpeg{info_cmd} -c copy "{out}"'
-    print(f'>>> {cmd}')
-    os.system(cmd)
+    for ext, video_list in video_dict.items():
+        print(f'Merge [{ext}] videos'.ljust(50, '='))
+        [print(i) for i in video_list]
+        input('Merge all above videos, press enter to continue...')
+        if ext == '.ts':
+            src = '+'.join([f'"{v}"' for v in video_list])
+            cmd = f'copy /b {src} "{out}"'
+        else:
+            info_cmd = ''.join([f' -i "{f}"' for f in video_dict])
+            cmd = f'ffmpeg{info_cmd} -c copy "{out}"'
+        print(f'{"+"*50}\n{cmd}\n{"+"*50}')
+        os.system(cmd)
 
 
 # ═══════════════════════════════════════════════
