aaf78b57456d1a958d84e987b69d7263bd5d9504
Erimus-Koo <chuangqianmingyueguang+github@gmail.com>
2020-09-25 23:43:33 +0800

    更新ts的拼接处理

--- m3u8_downloader.py
+++ m3u8_downloader.py
@@ -6,6 +6,7 @@ __author__ = 'Erimus'
 '''
 
 import os
+import re
 from erimus.toolbox import readClip, request, UA_PC, download, use_proxy
 import fire
 
@@ -13,6 +14,7 @@ import fire
 OUTPUT_PATH = 'G:\\Downloads\\'
 TEMP_PATH = os.path.join(OUTPUT_PATH, 'm3u8_download_temp')
 NAME = 'm3u8_download_temp'
+CMD_BASE = 'ffmpeg -f concat -safe 0 -protocol_whitelist "file,http,https,tcp,tls"'
 # ═══════════════════════════════════════════════
 
 
@@ -21,15 +23,19 @@ def time2sec(time_str):
     return int(ts[:2]) * 3600 + int(ts[2:4]) * 60 + int(ts[4:6])
 
 
-def m3u8_downloader(url=None, start=0, end=95959, OUTPUT_PATH=OUTPUT_PATH, NAME=NAME):
+def m3u8_downloader(url=None, start=0, end=95959,
+                    OUTPUT_PATH=OUTPUT_PATH, NAME=NAME, method='split'):
     url = url or readClip()  # 输入url 没有输入的话直接读取剪贴板
     print(f'{url = }')
     if not url.startswith('http') or 'm3u8' not in url:
         print('Please copy url of m3u8 first, then run this function.')
         return
 
+    # 格式化时间
     start = time2sec(start)
     end = time2sec(end)
+    # 输出文件目录
+    out = os.path.join(OUTPUT_PATH, f'{NAME}.mp4')
 
     url_without_param = url.split('?')[0] if '?' in url else url
     domain_here = '/'.join(url_without_param.split('/')[:-1]) + '/'  # 当前路劲
@@ -38,55 +44,75 @@ def m3u8_downloader(url=None, start=0, end=95959, OUTPUT_PATH=OUTPUT_PATH, NAME=
     print(f'{domain_root=}')
 
     body = request(url)
-    print(body)
+    print(f'body\n{body[:1000]}\n')
 
     # 如果获得的是一个清晰度跳转列表 跳转并获取媒体文件列表 但保持domain不变
     if '#EXTINF' not in body and '#EXT-X-STREAM-INF' in body:
-        last = ''
+        print('This is a m3u8 list!'.ljust(50, '='))
         lines = body.split('\n')
         for i, line in enumerate(lines):
             if line.startswith('#EXT-X-STREAM-INF'):
-                last = lines[i + 1]
-        body = request(domain_here + last)
-        print(body)
+                first = lines[i + 1]
+                break
+        print(f'Highest Resulution Address\n{domain_here + first}\n')
+        body = request(domain_here + first)
+        print(f'{"body".ljust(50,"=")}\n{body[:1000]}\n')
 
+    # 补全视频片段地址
     slice_list = []
-    now = 0
-    for line in body.split('\n'):
-        line = line.strip()
-        if not line.startswith('#') and start < now < end:
-            if not line.startswith('http'):  # 补全根目录
-                if line.startswith('/'):
-                    line = domain_root + line
-                else:
-                    line = domain_here + line
-            slice_list.append(line)
-        if line.startswith('#EXTINF:'):
-            now += float(line.strip('#EXTINF:,'))
+    now = 0  # 记录当前片段的起始秒数
+    ext = None
+    for line in re.findall(r'(?s)#EXTINF.*?(?=#)', body):
+        time, address = line.split('\n')[:2]
+        now += float(time.strip('#EXTINF:,'))
 
-    for index, url in enumerate(slice_list):
-        print(f'=== {index+1} / {len(slice_list)} ===')
-        download(url, TEMP_PATH, f'{NAME}_{index:04d}.ts')
+        if ext is None:
+            ext = address.split('?')[0].split('.')[-1]
 
-    temp_files = ''
-    for path, dirs, files in os.walk(TEMP_PATH):  # read all files
-        for fn in files:
-            if fn.startswith(NAME) and fn.split('.')[-1] in ['ts']:
-                file = os.path.join(path, fn)  # full path
-                temp_files += f"file '{file}'\n"
+        if not address.startswith('http'):  # 补全根目录
+            if address.startswith('/'):
+                full_address = domain_root + address
+            else:
+                full_address = domain_here + address
+        if start < now < end:
+            slice_list.append(full_address)
+            body = body.replace(address, full_address, 1)
+    print(f'{"new body".ljust(50,"=")}\n{body[:1000]}')
 
-    temp_videos = os.path.join(TEMP_PATH, f'{NAME}_videos.txt')
-    with open(temp_videos, 'w', encoding='utf-8') as f:
-        f.write(temp_files)
+    # 下载分片文件 并合并
+    if method == 'split':
+        # 下载分片
+        for index, url in enumerate(slice_list):
+            print(f'=== {index+1} / {len(slice_list)} ===')
+            download(url, TEMP_PATH, f'{NAME}_{index:04d}.{ext}')
 
-    out = os.path.join(OUTPUT_PATH, f'{NAME}.mp4')
-    cmd = f'ffmpeg -f concat -safe 0 -i "{temp_videos}" -c copy "{out}"'
-    print(f'>>> {cmd}')
+        temp_files = []
+        for path, dirs, files in os.walk(TEMP_PATH):  # read all files
+            for fn in files:
+                if fn.startswith(NAME) and fn.split('.')[-1] in [ext]:
+                    temp_files.append(os.path.join(path, fn))
+
+        if ext == 'ts':  # ts文件直接拼接
+            src = '+'.join([f'"{v}"' for v in temp_files])
+            cmd = f'copy /b {src} "{out}"'
+        else:  # 其他视频格式通过ffmpeg拼接
+            videos = ''.join([f' -i "{v}"' for v in temp_files])
+            cmd = f'{CMD_BASE}{videos} -c copy "{out}"'
+
+    # 直接用ffmpeg下载m3u8文件
+    elif method == 'direct':
+        m3u8_file = os.path.join(TEMP_PATH, f'{NAME}.m3u8')
+        with open(m3u8_file, 'w', encoding='utf-8') as f:
+            f.write(body)
+        cmd = f'{CMD_BASE} -i "{m3u8_file}" -c copy "{out}"'
+
+    print(f'{"+"*50}\n{cmd}\n{"+"*50}')
     os.system(cmd)
 
     if os.path.exists(out):
         print(f'Downlaod finished: {out}')
-        os.system(f'del {TEMP_PATH}\\{NAME}*.*')
+        if method == 'split':
+            os.system(f'del {TEMP_PATH}\\{NAME}*.*')
         os.system(f'rmdir {TEMP_PATH}')
 
 
