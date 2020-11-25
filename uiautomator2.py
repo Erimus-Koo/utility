15b09e98f7595103b547b0bc0451ea50b085957c
Erimus-Koo <chuangqianmingyueguang+github@gmail.com>
2020-11-26 00:30:27 +0800

    ++

---
+++ uiautomator2.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'Erimus'
+
+import uiautomator2
+import subprocess
+import re
+
+# ═══════════════════════════════════════════════
+
+# 检测adb设备ip
+def detect_adb_devices():
+    cmd = 'adb devices'
+    p = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+    out = p.stdout.decode('utf-8')
+    print(f'{out = }')
+    ports = re.findall(r'(?<=127\.0\.0\.1:)\d+', out)
+    print(f'{ports = }')
+    if ports:
+        return int(ports[0])
+
+
+# 获取模拟器
+def get_device(start_port=None):
+    # 这里的端口号可以用 adb devices 查看。
+    # 强制用端口号连接，有时候在 adb devices 找不到设备时仍可强制连接。
+    if start_port is None:
+        start_port = detect_adb_devices() or 62001
+
+    for port in range(start_port, 65535):
+        device = f'127.0.0.1:{port}'
+        try:
+            print(f'Try connect device: {device}')
+            d = uiautomator2.connect_adb_wifi(device)  # connect to device
+            break
+        except Exception:
+            pass
+    [print(f'{k}: {v}') for k, v in d.info.items()]
+    return d
+
+
+# 启动App
+# 获取游戏包名称
+# adb shell dumpsys window w |findstr \/ |findstr name=
+# 或者直接运行 然后看 current app
+def launch_app(d, package_name, force=False):
+    current = d.app_current().get('package')
+    print(f'Current App: {current}')
+    if current == package_name:
+        return
+
+    pid = None
+    while not pid or force:
+        print(f'Restart: {package_name}')
+        d.app_stop(package_name)
+        d.app_start(package_name)  # 未启动的话会启动
+        pid = d.app_wait(package_name)
+        if pid:
+            print(f'App [{package_name}] running in PID: {pid}')
+            return
+
+
+# ═══════════════════════════════════════════════
+
+
+if __name__ == '__main__':
+
+    detect_adb_devices()
+
+    # d = get_device()
+    # package_name = 'com.netease.cloudmusic'
+    # launch_app(package_name)
