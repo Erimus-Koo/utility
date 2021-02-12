f3034b7b9a201510a9f3d3eb078684a7cc3dbfbf
Erimus <chuangqianmingyueguang+github@gmail.com>
2021-02-13 01:37:41 +0800

    支持含空格的文件名

--- generate_docsify_sidebar.py
+++ generate_docsify_sidebar.py
@@ -6,46 +6,13 @@ __author__ = 'Erimus'
 # export '_sidebar.md' & '_sidebar_private.md', toggle by some private key.
 
 import os
+from urllib.parse import quote, unquote
 
 # ═══════════════════════════════════════════════
-md = md_private = ''
-# ═══════════════════════════════════════════════
-
-
-def add_content(fullpath):
-    global md, md_private
-    fullpath = fullpath.replace('\\', '/')
-    fullpath = fullpath[len(root):].lstrip('/')
-    if not fullpath:
-        return  # skip root
-    indent = fullpath.count('/') * '    '  # indent level
-    levels = f'/{fullpath}'.split('/')  # per folder in path
-    filename = levels[-1]  # get file/folder name
-    # ignore sidebar file
-    if filename.startswith('_sidebar') or '.assets' in fullpath:
-        return
-    filename = filename[0] + filename[1:].replace('_', ' ')
-
-    # generate line
-    if fullpath.endswith('.md'):
-        line = f'{indent}- [**{filename[:-3]}**]({fullpath})'
-    else:  # folder
-        line = f'{indent}- **{filename}**'
-
-    # add line to public md
-    for name in levels:
-        if name.startswith('_'):
-            break
-    else:  # all path(self or parent) is not private
-        md += f'{line}\n'
-
-    # add line to private
-    line = line.replace('**_', '**')  # remove private underscore in display
-    md_private += f'{line}\n'
 
 
 def update_file(file, content):
-    # read old file
+    # read old file. If changes, update file.
     old = ''
     if os.path.exists(file):
         with open(file, 'r', encoding='utf-8') as f:
@@ -61,26 +28,79 @@ def update_file(file, content):
         print(f'{fn} - has no change')
 
 
+def add_content(fullpath, root):
+    print(f'add content: {fullpath}')
+
+    # calculate indent
+    relative_path = fullpath.replace('\\', '/')[len(root):].lstrip('/')
+    print(f'{relative_path = }')
+    indent = relative_path.count('/') * '    '  # indent level
+    levels = f'/{relative_path}'.split('/')  # per folder in path
+
+    if not relative_path:
+        return 'error', 'root'  # skip root
+
+    # get file/folder name
+    filename = levels[-1]
+    # replace '_' with space in sidebar
+    filename = filename[0] + filename[1:].replace('_', ' ')
+    print(f'{filename = }')
+
+    # skip sidebar file
+    if filename.startswith('_sidebar') and fullpath.endswith('.md'):
+        return 'error', 'sidebar.md'
+
+    # generate line
+    _type = 'public'  # if sidebar authority private
+
+    if fullpath.endswith('.md'):
+        # generate file entrance.
+        filename = filename.lstrip("_")[:-3]  # remove '_' and '.md'
+        line = f'{indent}- [**{filename}**]({quote(relative_path)})\n'
+    else:  # folder
+        line = f'{indent}- **{filename}**\n'
+
+    # filter private path or file (starts with '_')
+    for name in levels:
+        if name.startswith('_'):
+            _type = 'private'
+            break
+
+    print(f'{_type = } | {line}')
+    return _type, line
+
+
 def generate_sidebar(root=None, ignoreFolders=None):
-    global md, md_private
     ignoreFolders = [] if ignoreFolders is None else ignoreFolders
     ignoreFolders += ['.git']
     if root is None:
         root = os.getcwd()  # the path of command line
 
-    md = md_private = ''
+    md = md_private = ''  # markdown file content
 
     for path, dirs, files in os.walk(root):  # read all files
         dirs[:] = [d for d in dirs if d not in ignoreFolders]
-        path_wrote = False
+
+        if (path.endswith('.assets')):  # skip assets folder
+            continue
+
+        # add folder name to menu
+        _type, line = add_content(path, root)
+        if _type in ['public', 'private']:
+            if _type == 'public':
+                md += line
+            md_private += line
+
+        # add file to menu
         for fn in files:
             if fn.endswith('md'):
-                # if folder is empty, it need not to be write.
-                if not path_wrote and not fn.startswith('_'):
-                    add_content(path)
-                    path_wrote = True
-                # add md file
-                add_content(os.path.join(path, fn))
+                print(f'===\n{fn = }')
+                # generate sidebar
+                _type, line = add_content(os.path.join(path, fn), root)
+                if _type in ['public', 'private']:
+                    if _type == 'public':
+                        md += line
+                    md_private += line
 
     # print(f'_sidebar.md\n{md}\n-----\n\n_sidebar_private.md\n{md_private}')
 
@@ -95,6 +115,6 @@ if __name__ == '__main__':
     DOCSIFY_ROOT = 'D:/OneDrive/site/notebook'
     for root, ignoreFolders in [
         [DOCSIFY_ROOT, ['shanghaitong']],  # docsify root
-        [DOCSIFY_ROOT + '/shanghaitong', []]  # 上海地方志
+        # [DOCSIFY_ROOT + '/shanghaitong', []]  # 上海地方志
     ]:
         generate_sidebar(root, ignoreFolders)
